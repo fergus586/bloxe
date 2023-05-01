f772580d7c6a8c93039e780a5dd17fed885c6e08
fergus586 <72260378+fergus586@users.noreply.github.com>
2023-05-01 11:20:55 +0100

    background updates

--- src/App.js
+++ src/App.js
@@ -1,39 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import ProtectedContent from './ProtectedContent';
 
-function App() {
+const App = () => {
+  const [password, setPassword] = useState('');
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const correctPassword = 'test'; // Replace with your desired password
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (password === correctPassword) {
+      setIsLoggedIn(true);
+    } else {
+      alert('Incorrect password. Please try again.');
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Welcome to Bloxe!</h1>
-      </header>
-      <main>
-        <section>
-          <h2>About Us</h2>
-          <p>
-            Bloxe is a cutting-edge record label showcasing the best talent from around the world. We're dedicated to promoting and supporting our artists, and connecting them with fans who appreciate great music.
-          </p>
-        </section>
-        <section>
-          <h2>Our Artists</h2>
-          <p>
-            We represent a diverse group of artists across various genres. Explore our roster and discover your new favorite musicians.
-          </p>
-        </section>
-        <section>
-          <h2>Latest Releases</h2>
-          <p>
-            Stay up-to-date with the latest albums, singles, and remixes from our artists. Don't miss a beat – check out our newest releases now!
-          </p>
-        </section>
-      </main>
-      <footer>
-        <p>
-          &copy; {new Date().getFullYear()} Bloxe. All rights reserved.
-        </p>
-      </footer>
+      {isLoggedIn ? (
+        <ProtectedContent />
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="password">Enter the password to access music downloads:</label>
+          <br />
+          <input
+            type="password"
+            id="password"
+            className="password-input"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <br />
+          <button type="submit">Submit</button>
+        </form>
+      )}
     </div>
   );
-}
+};
 
 export default App;
