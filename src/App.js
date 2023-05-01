8247dc9adca58953a510f3e6883edcb2c82a153a
fergus586 <72260378+fergus586@users.noreply.github.com>
2023-05-01 11:24:13 +0100

    updates

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const App = () => {
   const [password, setPassword] = useState('');
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  const correctPassword = 'test'; // Replace with your desired password
+  const correctPassword = 'TEST'; // Replace with your desired password
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -22,19 +22,22 @@ const App = () => {
       {isLoggedIn ? (
         <ProtectedContent />
       ) : (
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="password">Enter the password to access music downloads:</label>
-          <br />
-          <input
-            type="password"
-            id="password"
-            className="password-input"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <br />
-          <button type="submit">Submit</button>
-        </form>
+        <>
+          <div className="blur-overlay" />
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="password">Enter the password to access music downloads:</label>
+            <br />
+            <input
+              type="password"
+              id="password"
+              className="password-input"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <br />
+            <button type="submit">Submit</button>
+          </form>
+        </>
       )}
     </div>
   );
