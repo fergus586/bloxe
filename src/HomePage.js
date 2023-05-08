1212a5f2f3b225c02d292cb884888866f08cc15a
fergus586 <72260378+fergus586@users.noreply.github.com>
2023-05-08 14:40:15 +0100

    build

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from './Navbar';
 import './HomePage.css';
 
 const HomePage = () => {
+  const [active, setActive] = useState(false);
+
+  const handleMouseOver = () => {
+    setActive(true);
+  };
+
   return (
     <div>
       <Navbar />
       <div className="home-content">
-        <h1><span>BLOXE</span>NOT BLOKE</h1>
+        <h1>
+          <span
+            onMouseOver={handleMouseOver}
+            className={active ? 'image-active' : ''}
+          >
+            BLOXE
+          </span>{''}
+        NOT BLOKE
+        </h1>
       </div>
     </div>
   );
