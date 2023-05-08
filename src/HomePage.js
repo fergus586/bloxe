c8309deea925f7a959a9b19bf62d6e203f653dda
fergus586 <72260378+fergus586@users.noreply.github.com>
2023-05-08 13:07:20 +0100

    updates to home

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,29 +1,13 @@
 import React from 'react';
 import Navbar from './Navbar';
-import InteractiveImage from './InteractiveImage';
 import './HomePage.css';
 
 const HomePage = () => {
-  const images = [
-    {
-      src: '/ReleaseArt/release1.png',
-      alt: 'Release 1',
-    },
-    ...Array(8).fill({ isComingSoon: true }),
-  ];
-
   return (
     <div>
       <Navbar />
-      <div className="home-grid">
-        {images.map((image, index) => (
-          <InteractiveImage
-            key={index}
-            src={image.src}
-            alt={image.alt}
-            isComingSoon={image.isComingSoon}
-          />
-        ))}
+      <div className="home-content">
+        <h1><span>BLOXE</span>NOT BLOKE</h1>
       </div>
     </div>
   );
