45a483a5370657d71ce41a5653dd631914fd0553
Joe Pryor <Joe.pryor9@gmail.com>
2023-12-28 15:44:51 +0000

    Test

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import PasswordPage from './PasswordPage';
 import HomePage from './HomePage';
 import { BrowserRouter, Route, Navigate, Routes } from 'react-router-dom';
 
 const App = () => {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState(
+    localStorage.getItem('isAuthenticated') === 'true'
+  );
 
   const handlePasswordSuccess = () => {
+    localStorage.setItem('isAuthenticated', 'true');
+    console.log('password success');
     setIsAuthenticated(true);
   };
+
+  useEffect(() => {
+    const authStatus = localStorage.getItem('isAuthenticated') === 'true';
+    console.log('authStatus', authStatus);
+    setIsAuthenticated(authStatus);
+  }, []);
 
   return (
     <div className="App">
       <BrowserRouter>
-        {isAuthenticated ? (
-          <Navigate to="/home" />
-        ) : null}
+        {isAuthenticated ? <Navigate to="/home" /> : null}
         <Routes>
           <Route path="/" element={<PasswordPage onPasswordSuccess={handlePasswordSuccess} />} />
           <Route path="/home" element={<HomePage />} />
